ff3f25b420e1730bec59844ab791f2967854d985
남정욱 <wjddnr3304@gmail.com>
2024-10-16 13:14:33 +0900

    초기 커밋.

---
+++ Lock_EX/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Lock_EX
+{
+    internal class Program
+    {
+        static int num = 0;
+        static object _lock = new object();
+
+        static void Thread_1()
+        {
+            for (int i = 0; i < 100000; ++i)
+            {
+                lock (_lock)
+                {
+                    num++;
+                }
+            }
+        }
+        static void Thread_2()
+        {
+            for(int i = 0;i <100000l ; ++i)
+            {
+                Monitor.Enter(_lock);
+                num--;
+                Monitor.Exit(_lock);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(Thread_1);
+            Thread t2 = new Thread(Thread_2);
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+            Console.WriteLine(num);
+        }
+    }
+}
